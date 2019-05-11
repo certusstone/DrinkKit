65cf8191c8b7368cc71f81bc6214771922ad071a
certusstone <1507053+certusstone@users.noreply.github.com>
2019-05-10 20:01:00 -0500

    Initial Commit

---
+++ DrinkKit/DrinkKit.h
@@ -0,0 +1,17 @@
+//
+// DrinkKit.h
+//
+// Created: 5/10/19
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrinkKit.
+FOUNDATION_EXPORT double DrinkKitVersionNumber;
+
+//! Project version string for DrinkKit.
+FOUNDATION_EXPORT const unsigned char DrinkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrinkKit/PublicHeader.h>
+
+
